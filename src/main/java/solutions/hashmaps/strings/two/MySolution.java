d66b51e906289e5b7f102f59ffe3e786c1f60413
Danil Pogozhev <danilpogozhev@gmail.com>
2021-03-10 16:49:31 +0300

    Two Strings: Solved challenge.

--- src/main/java/solutions/hashmaps/strings/two/MySolution.java
+++ src/main/java/solutions/hashmaps/strings/two/MySolution.java
@@ -1,2 +1,21 @@
-package solutions.hashmaps.strings.two;public class MySolution {
+package solutions.hashmaps.strings.two;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MySolution {
+    static String twoStrings(String s1, String s2) {
+        Set<Character> characters = new HashSet<>();
+        for (Character c : s1.toCharArray()) {
+            characters.add(c);
+        }
+
+        for (Character c : s2.toCharArray()) {
+            if (characters.contains(c)) {
+                return "YES";
+            }
+        }
+
+        return "NO";
+    }
 }
