acc6424022a622d0d965e7bdd3d79eb63fd6deef
Danil Pogozhev <danilpogozhev@gmail.com>
2021-03-28 13:19:04 +0300

    LeetCode: LinkedList 1290.

---
+++ src/main/java/leetcode/linkedlist/Task1290.java
@@ -0,0 +1,30 @@
+package leetcode.linkedlist;
+
+import leetcode.linkedlist.support.ListNode;
+
+/*
+ * https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+ */
+public class Task1290 {
+    public int getDecimalValue(ListNode head) {
+        StringBuilder temp = new StringBuilder();
+
+        while (true) {
+            if (head == null) {
+                break;
+            } else {
+                temp.append(head.val);
+                head = head.next;
+            }
+        }
+
+        int counter = 1;
+        int result = 0;
+        for (int i = temp.length() - 1; i >=0; i--) {
+            result += counter * Integer.parseInt(temp.substring(i, i + 1));
+            counter *= 2;
+        }
+
+        return result;
+    }
+}
