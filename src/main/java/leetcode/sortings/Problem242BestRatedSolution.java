0d7d42c1d6efd9b8cacf43cc671556e6aef6646c
Danil Pogozhev <danilpogozhev@gmail.com>
2021-04-04 22:49:41 +0300

    LeetCode: Sort problem #922. Added best rated solution.

--- src/main/java/leetcode/sortings/Problem242BestRatedSolution.java
+++ src/main/java/leetcode/sortings/Problem242BestRatedSolution.java
@@ -6,13 +6,11 @@ package leetcode.sortings;
  * Author URL:   https://leetcode.com/xhadfasd/
  */
 public class Problem242BestRatedSolution {
-    public class Solution {
-        public boolean isAnagram(String s, String t) {
-            int[] alphabet = new int[26];
-            for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-            for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
-            for (int i : alphabet) if (i != 0) return false;
-            return true;
-        }
+    public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
     }
 }
--- src/main/java/leetcode/sortings/Problem922.java
+++ src/main/java/leetcode/sortings/Problem922.java
@@ -18,6 +18,33 @@ public class Problem922 {
                 odd += 2;
             }
         }
+
         return result;
+    }
+
+    /**
+     * Author URL: https://leetcode.com/user2406z/
+     * Solution URL: https://leetcode.com/problems/sort-array-by-parity-ii/discuss/181500/Java-Two-pointers-inplace-O(n)-time-simple-and-straightforward
+     */
+    public int[] bestRatedSolution(int[] A) {
+        int e = 0;
+        int o = 1;
+
+        while (e < A.length && o < A.length) {
+            if (A[e] % 2 != 0) {
+                swap(A, e, o);
+                o += 2;
+            } else {
+                e += 2;
+            }
+        }
+
+        return A;
+    }
+
+    private void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 }
